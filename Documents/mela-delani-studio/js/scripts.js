6775ecb0328ebcbc57fb15a78954ba801baf3371
Nyokabi12 <sylvianyokabi536@gmail.com>
2019-10-01 09:31:40 +0300

    add toggle function

--- Documents/mela-delani-studio/js/scripts.js
+++ Documents/mela-delani-studio/js/scripts.js
@@ -4,5 +4,13 @@ $(document).ready(function(){
          $(this).children(".hidden").slideToggle();
          $(this).children("#design img").slideToggle();
      });
-     
+     // $("#part2,#development").click(function(){
+     //     $("#part2").slideToggle();
+     //     $("#development").slideToggle();
+     // });
+     // $("#part3,#product").click(function(){
+     //     $("#part3").slideToggle();
+     //     $("#product").slideToggle();
+    //  });
   
+    
